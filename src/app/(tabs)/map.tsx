66ac95120fe8e10f1b6ab7a888ef4d365446841a
Lucas Henrique <forlucashenrique@gmail.com>
2024-08-29 07:43:35 -0300

    add native maps

--- src/app/(tabs)/map.tsx
+++ src/app/(tabs)/map.tsx
@@ -1,9 +1,14 @@
 import { Text, View } from "react-native";
+import MapView, { UrlTile } from "react-native-maps";
 
 export default function SettingsScreen () {
     return (
         <View>
-            <Text>Map</Text>
+            <MapView 
+            
+            >
+                <UrlTile urlTemplate="https://api.maptiler.com/maps/4727213f-c0cb-4cc9-91db-a21acb678f2a/style.json?key=pUYYvwuqRAzZuxfmqfP9"/>
+            </MapView>
         </View>
     )
 }
