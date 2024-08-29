211d00070950bba0ca243aa9a501757e355c25e7
Lucas Henrique <forlucashenrique@gmail.com>
2024-08-29 08:17:15 -0300

    add mapbox lib

--- src/app/(tabs)/map.tsx
+++ src/app/(tabs)/map.tsx
@@ -1,13 +1,14 @@
 import { Text, View } from "react-native";
-import MapView, { UrlTile } from "react-native-maps";
+
+import { MapView } from "@rnmapbox/maps";
 
 export default function SettingsScreen () {
     return (
         <View>
-            <MapView 
-            
+            <MapView
+                styleURL=""
             >
-                <UrlTile urlTemplate="https://api.maptiler.com/maps/4727213f-c0cb-4cc9-91db-a21acb678f2a/style.json?key=pUYYvwuqRAzZuxfmqfP9"/>
+
             </MapView>
         </View>
     )
